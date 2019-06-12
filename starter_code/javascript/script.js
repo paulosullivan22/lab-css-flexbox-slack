debef26337e3291281c11f3e7863ce13b760f21b
paul <paulosullivan22@outlook.com>
2019-06-12 09:59:18 +0200

    built front end

---
+++ starter_code/javascript/script.js
@@ -0,0 +1,11 @@
+var coll = document.querySelector(".collapsible");
+
+coll.addEventListener("click", function() {
+  this.classList.toggle("active");
+   var content = this.nextElementSibling;
+   if (content.style.display === "block") {
+     content.style.display = "none";
+   } else {
+     content.style.display = "block";
+   }
+  });
